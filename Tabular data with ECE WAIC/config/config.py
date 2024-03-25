b4c9f5d994ae9b47b7f1e1811b9dc7fda1aa8f15
LarsELund <69896435+LarsELund@users.noreply.github.com>
2024-03-25 13:06:39 +0100

    Add files via upload

---
+++ Tabular data with ECE WAIC/config/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+config = {}
+config['num_epochs' ] = 250
+config['test_samples'] = 1000
+config['lr'] = 0.005
+config['num_transforms'] = 2
+config['hidden_dim'] = 500
